caf7cf0e41cc94f695208d08e7416ed34ca10c0a
Jonathan Narvaez <jonaser06@gmail.com>
2019-03-02 17:44:47 -0500

    comentario

--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <div class="row">
         <div class="col-12">
             <h1>Hola mundo2</h1>
+            <h1>Hola mundo2</h1>
+            <h1>Hola mundo2</h1>
         </div>
     </div>
     <div class="row">
